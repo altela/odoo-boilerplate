dc190e31028ab2e71fede0002e8107432804fc26
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-03-14 16:50:35 +0800

    Added new tab module, which later can be used to add a notebook (a tab) inside a view

---
+++ m_add_newtab/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Add New Tab Boilerplate Example',
+    'author': 'Altela Eleviansyah Pramardhika',
+    'version': '12.0.0.1.0',
+    'license': 'LGPL-3',
+    'summary': 'Boilerplate & example for creating a new tab in inherited views',
+    'sequence': 1,
+    'description': """Boilerplate & example for creating a new tab in inherited views""",
+    'category': 'Sales',
+    'website': 'https://altela.my.id',
+    'depends': ["stock"],
+    'data': [
+        'views/newtab.xml'
+    ],
+    
+    'images': [
+    ],
+
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ m_add_newtab/models/newtab.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+
+
+class NewTab(models.Model):
+    _inherit = "product.template"
+
+
