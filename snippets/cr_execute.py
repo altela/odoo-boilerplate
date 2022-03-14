dfc3b597ac596d59b7e77126c1c39d430bfbe280
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-03-14 16:29:41 +0800

    Added comment to explain defining an object that later can be used in cr

--- snippets/cr_execute.py
+++ snippets/cr_execute.py
@@ -1,2 +1,4 @@
+# This is the example of defining object from self and using cr (cursor) from psycopg connection to do queries
+
 picking_id = self.id
 self.env.cr.execute("SELECT name FROM stock_picking WHERE sale_id = %s", [picking_id])
